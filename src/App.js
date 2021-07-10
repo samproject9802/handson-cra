c68bc6f7aec89cb528161bc1ef90e33e564b0741
samproject9802 <manurungsamuel9802@gmail.com>
2021-07-10 20:16:06 +0700

    first API Setting commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log(process.env.REACT_APP_GIPHY_KEY);
+
   return (
     <div className="App">
       <header className="App-header">
